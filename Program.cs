09154c3ec088e347c1de3913b3dcd43661471721
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-09-29 23:07:29 -0300

    Add usings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("Hello World");
